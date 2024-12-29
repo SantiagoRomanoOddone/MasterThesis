633218a13ab4cc4124ccee1582c8fcad1aa11634
SantiagoRomanoOddone <santiagoromano15@gmail.com>
2024-12-28 23:23:53 -0300

    analyzing cluster 1

--- models/main.py
+++ models/main.py
@@ -8,10 +8,10 @@ import pandas as pd
 
 
 if __name__ == '__main__':
-    # Compare the results of the models for cluster 3
-    catboost_results = analyze_catboost(3)
-    xgboost_results = analyze_xgboost(3)
-    mean_results = analyze_mean(3)
+    # Compare the results of the models for a specific cluster
+    catboost_results = analyze_catboost(1)
+    xgboost_results = analyze_xgboost(1)
+    mean_results = analyze_mean(1)
 
     catboost_mean_mse = np.mean([result['mse'] for result in catboost_results])
     catboost_mean_rmse = np.mean([result['rmse'] for result in catboost_results])
@@ -51,5 +51,16 @@ if __name__ == '__main__':
     # Optionally, save the results to a DataFrame or CSV file
     best_models_df = pd.DataFrame(best_models_list)
     print(best_models_df['best_model'].value_counts())
+    # CatBoost Mean MSE: 5129869189.875176
+    # CatBoost Mean RMSE: 3415.917703206119
+    # XGBoost Mean MSE: 3584176817.65485
+    # XGBoost Mean RMSE: 2611.38865951491
+    # Mean Sale Mean MSE: 8874580619.746906
+    # Mean Sale Mean RMSE: 10080.256686764185
+
+    
+    # best_model
+    # XGBoost     5900
+    # CatBoost    1154
 
     print("[END] Model Comparison")
