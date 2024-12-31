0d66a6b74d930da667010c24ec88c296f3a2ba15
SantiagoRomanoOddone <santiagoromano15@gmail.com>
2024-12-31 13:56:39 -0300

    main testing in progess

--- models/main.py
+++ models/main.py
@@ -1,6 +1,9 @@
-from models.catboost.main import analyze_catboost
-from models.mean_sale.main import analyze_mean
-from models.xgboost.main import analyze_xgboost
+from models.catboost.main import catboost, catboost_by_product
+# from models.mean_sale.main import analyze_mean
+from models.xgboost.main import xgboost, xgboost_by_product
+from train.splits.fixed_split import fixed_split
+from train.transformations.onehot_encoding_pdv import onehot_encoding_pdv
+from metrics.metrics import Metrics
 import numpy as np
 import pandas as pd
 
@@ -8,58 +11,30 @@ import pandas as pd
 
 
 if __name__ == '__main__':
-    # Compare the results of the models for a specific cluster
-    catboost_results = analyze_catboost(3)
-    xgboost_results = analyze_xgboost(3)
-    mean_results = analyze_mean(3)
 
-    catboost_mean_mse = np.mean([result['mse'] for result in catboost_results])
-    catboost_mean_rmse = np.mean([result['rmse'] for result in catboost_results])
-    print(f"CatBoost Mean MSE: {catboost_mean_mse}")
-    print(f"CatBoost Mean RMSE: {catboost_mean_rmse}")
+    cluster_number = 3
 
-    xgboost_mean_mse = np.mean([result['mse'] for result in xgboost_results])
-    xgboost_mean_rmse = np.mean([result['rmse'] for result in xgboost_results])
-    print(f"XGBoost Mean MSE: {xgboost_mean_mse}")
-    print(f"XGBoost Mean RMSE: {xgboost_mean_rmse}")
+    features = pd.read_parquet('features/processed/features.parquet').sort_values(['pdv_codigo', 'codigo_barras_sku', 'fecha_comercial']).reset_index(drop=True)
 
-    mean_mean_mse = np.mean([result['mse'] for result in mean_results])
-    mean_mean_rmse = np.mean([result['rmse'] for result in mean_results])
-    print(f"Mean Sale Mean MSE: {mean_mean_mse}")
-    print(f"Mean Sale Mean RMSE: {mean_mean_rmse}")
+    features = features[features['cluster'] == cluster_number]
+    
+    train_df, test_df = fixed_split(features)
+    
+    # Testing catboost models 
+    cb_results = catboost(features)
+    cb_results_sku = catboost_by_product(features)
+    xgb_results = xgboost(features)
+    xgb_results_sku = xgboost_by_product(features)
 
+    test_df = pd.merge(test_df, cb_results, on=['pdv_codigo', 'codigo_barras_sku', 'fecha_comercial','cant_vta'], how='left')
+    test_df = pd.merge(test_df, cb_results_sku, on=['pdv_codigo', 'codigo_barras_sku', 'fecha_comercial','cant_vta'], how='left')
+    test_df = pd.merge(test_df, xgb_results, on=['pdv_codigo', 'codigo_barras_sku', 'fecha_comercial','cant_vta'], how='left')
+    test_df = pd.merge(test_df, xgb_results_sku, on=['pdv_codigo', 'codigo_barras_sku', 'fecha_comercial','cant_vta'], how='left')
 
-    all_results = catboost_results + xgboost_results + mean_results
+    summary_df = Metrics().create_summary_dataframe(test_df)
 
-    best_models = {}
-
-    # Iterate through all results and find the best model for each combination
-    for result in all_results:
-        key = (result['pdv_codigo'], result['codigo_barras_sku'])
-        if key not in best_models or result['rmse'] < best_models[key]['rmse']:
-            best_models[key] = {
-                'pdv_codigo': result['pdv_codigo'],
-                'codigo_barras_sku': result['codigo_barras_sku'],
-                'mse': result['mse'],
-                'rmse': result['rmse'],
-                'best_model': result['model']
-            }
-
-    best_models_list = list(best_models.values())
-
-    best_models_df = pd.DataFrame(best_models_list)
-    print(best_models_df['best_model'].value_counts())
+    print(summary_df['best_rmse'].value_counts())
+    print(summary_df['best_mse'].value_counts())
 
 
 
-    # best_model cluster 1 
-    # CatBoost Mean MSE: 5129869189.875176
-    # CatBoost Mean RMSE: 3415.917703206119
-    # XGBoost Mean MSE: 3584176817.65485
-    # XGBoost Mean RMSE: 2611.38865951491
-    # Mean Sale Mean MSE: 8874580619.746906
-    # Mean Sale Mean RMSE: 10080.256686764185
-    # XGBoost     5900
-    # CatBoost    1154
-
-    print("[END] Model Comparison")
