3d96497196729c3b065d2f56f51ee18a6748cd96
SantiagoRomanoOddone <santiagoromano15@gmail.com>
2025-02-19 08:06:27 -0300

    adding ligtgbm

---
+++ models/lightgbm/lightgbm_main.py
@@ -0,0 +1,99 @@
+import pandas as pd
+import numpy as np
+from metrics.metrics import Metrics
+from models.lightgbm.lightgbm_model import LightGBMRegressor
+from train.splits.fixed_split import fixed_split
+from train.transformations.onehot_encoding_pdv import onehot_encoding_pdv
+
+def lightgbm(cluster_data):
+    print('[START] LightGBM model')
+
+    combinations = cluster_data[['pdv_codigo', 'codigo_barras_sku']].drop_duplicates()
+
+    results = []
+
+    for _, row in combinations.iterrows():
+        pdv_codigo = row['pdv_codigo']
+        codigo_barras_sku = row['codigo_barras_sku']
+        print(f"Processing pdv_codigo: {pdv_codigo}, codigo_barras_sku: {codigo_barras_sku}")
+
+        data = cluster_data[(cluster_data['codigo_barras_sku'] == codigo_barras_sku) & (cluster_data['pdv_codigo'] == pdv_codigo)]
+        
+        # Split
+        train_df, test_df = fixed_split(data)
+        if train_df.empty or test_df.empty:
+            continue
+    
+        if train_df['cant_vta'].nunique() == 1:
+            print(f"Skipping due to equal train targets")
+            continue
+        
+        # Train a LightGBM model
+        model = LightGBMRegressor()
+        model.fit(train_df)
+        # Make predictions
+        result = model.predict(test_df)
+
+        results.append(result)
+
+    final_results = pd.concat(results, ignore_index=True)
+    final_results.rename(columns={'cant_vta_pred': 'cant_vta_pred_lgbm_pdv_sku'}, inplace=True)
+    print('[END] LightGBM model')
+    return final_results
+
+
+def lightgbm_by_product(cluster_data):
+    print('[START] LightGBM model by product')
+
+    products = cluster_data['codigo_barras_sku'].unique()
+    results = []
+
+    for codigo_barras_sku in products:
+        print(f"Processing codigo_barras_sku: {codigo_barras_sku}")
+
+        data = cluster_data[cluster_data['codigo_barras_sku'] == codigo_barras_sku]
+        data = onehot_encoding_pdv(data)
+        # Split
+        train_df, test_df = fixed_split(data)
+        if train_df.empty or test_df.empty:
+            continue
+    
+        if train_df['cant_vta'].nunique() == 1:
+            print(f"Skipping codigo_barras_sku: {codigo_barras_sku} due to equal train targets")
+            continue
+
+        # Train a LightGBM model
+        model = LightGBMRegressor()
+        model.fit(train_df)
+        # Make predictions
+        result = model.predict(test_df)
+
+        results.append(result)
+
+    final_results = pd.concat(results, ignore_index=True)
+    final_results.rename(columns={'cant_vta_pred': 'cant_vta_pred_lgbm_sku'}, inplace=True)
+    print('[END] LightGBM model by product')
+    return final_results
+
+
+if __name__ == '__main__':
+    
+    cluster_number = 3
+
+    features = pd.read_parquet('features/processed/features.parquet').sort_values(['pdv_codigo', 'codigo_barras_sku', 'fecha_comercial']).reset_index(drop=True)
+
+    features = features[features['cluster'] == cluster_number]
+    
+    train_df, test_df = fixed_split(features)
+    
+    # Testing catboost models 
+    results = lightgbm(features)
+    results_sku = lightgbm_by_product(features)
+
+    test_df = test_df.merge(results_sku, on=['codigo_barras_sku', 'pdv_codigo','fecha_comercial','cant_vta'], how='left')
+    test_df = test_df.merge(results, on=['codigo_barras_sku', 'pdv_codigo','fecha_comercial','cant_vta'], how='left')
+
+    summary_df = Metrics().create_summary_dataframe(test_df)
+
+    print(summary_df['best_rmse'].value_counts())
+    print(summary_df['best_mse'].value_counts())
--- models/main.py
+++ models/main.py
@@ -2,6 +2,7 @@ from models.catboost.catboost_main import catboost, catboost_by_product
 from models.mean_sale.mean_sale_main import mean_sale
 from models.xgboost.xgboost_main import xgboost, xgboost_by_product
 from models.deep_learning.lstm.lstm_main import lstm
+from models.lightgbm.lightgbm_main import lightgbm, lightgbm_by_product
 from train.splits.fixed_split import fixed_split
 from train.transformations.onehot_encoding_pdv import onehot_encoding_pdv
 import matplotlib.pyplot as plt
@@ -31,6 +32,8 @@ def plot_combinations(data, num_combinations):
         plt.plot(subset['fecha_comercial'], subset['cant_vta_pred_cb_sku'], label='CB SKU Prediction')
         plt.plot(subset['fecha_comercial'], subset['cant_vta_pred_xgb_pdv_sku'], label='XGB PDV SKU Prediction')
         plt.plot(subset['fecha_comercial'], subset['cant_vta_pred_xgb_sku'], label='XGB SKU Prediction')
+        plt.plot(subset['fecha_comercial'], subset['cant_vta_pred_lgbm_pdv_sku'], label='LGBM PDV SKU Prediction')
+        plt.plot(subset['fecha_comercial'], subset['cant_vta_pred_lgbm_sku'], label='LGBM SKU Prediction')
         plt.plot(subset['fecha_comercial'], subset['cant_vta_pred_mean_pdv_sku'], label='Mean PDV SKU Prediction')
         # plt.plot(subset['fecha_comercial'], subset['cant_vta_pred_lstm_pdv_sku'], label='LSTM PDV SKU Prediction')
         
@@ -45,7 +48,7 @@ def plot_combinations(data, num_combinations):
 
 if __name__ == '__main__':
 
-    cluster_number = 3
+    cluster_number = 0
     features = pd.read_parquet('features/processed/features.parquet').sort_values(['pdv_codigo', 'codigo_barras_sku', 'fecha_comercial']).reset_index(drop=True)
 
     features = features[features['cluster'] == cluster_number]
@@ -57,12 +60,17 @@ if __name__ == '__main__':
     xgb_results = xgboost(features)
     xgb_results_sku = xgboost_by_product(features)
     mean_sale_results = mean_sale(features)
+    lgbm_results = lightgbm(features)
+    lgbm_results_sku = lightgbm_by_product(features)
     # lstm_results = lstm(features)
 
     test_df = pd.merge(test_df, cb_results, on=['pdv_codigo', 'codigo_barras_sku', 'fecha_comercial','cant_vta'], how='left')
     test_df = pd.merge(test_df, cb_results_sku, on=['pdv_codigo', 'codigo_barras_sku', 'fecha_comercial','cant_vta'], how='left')
     test_df = pd.merge(test_df, xgb_results, on=['pdv_codigo', 'codigo_barras_sku', 'fecha_comercial','cant_vta'], how='left')
     test_df = pd.merge(test_df, xgb_results_sku, on=['pdv_codigo', 'codigo_barras_sku', 'fecha_comercial','cant_vta'], how='left')
+    test_df = pd.merge(test_df, lgbm_results, on=['pdv_codigo', 'codigo_barras_sku', 'fecha_comercial','cant_vta'], how='left')
+    test_df = pd.merge(test_df, lgbm_results_sku, on=['pdv_codigo', 'codigo_barras_sku', 'fecha_comercial','cant_vta'], how='left')
+
     test_df = pd.merge(test_df, mean_sale_results, on=['pdv_codigo', 'codigo_barras_sku', 'fecha_comercial','cant_vta'], how='left')
     # test_df = pd.merge(test_df, lstm_results, on=['pdv_codigo', 'codigo_barras_sku', 'fecha_comercial','cant_vta'], how='left')
 
