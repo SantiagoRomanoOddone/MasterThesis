645498c991e205560e5cded5cdab947cb9ac0d98
SantiagoRomanoOddone <santiagoromano15@gmail.com>
2025-03-12 08:11:25 -0300

    adding feedforward model

--- models/deep_learning/simple_feedforward/simple_feedforward.py
+++ models/deep_learning/simple_feedforward/simple_feedforward.py
@@ -1,10 +1,11 @@
-from gluonts.torch import DeepAREstimator
+from gluonts.torch import SimpleFeedForwardEstimator
 from models.deep_learning.gluonts.functions import (check_data_requirements, 
                                                     set_random_seed, 
                                                     prepare_dataset, 
                                                     create_list_dataset,
                                                     make_predictions)
 
+import pandas as pd
 import numpy as np
 import random
 random_seed = 42
@@ -14,9 +15,6 @@ np.random.seed(random_seed)
 import pandas as pd
 import numpy as np
 
-
-
-
 CLUSTER_NUMBER = 3
 FREQ = "D"
 PREDICTION_LENGTH = 30
@@ -24,56 +22,37 @@ START_TRAIN = pd.Timestamp("2022-12-01")
 START_TEST = pd.Timestamp("2024-11-01")
 END_TEST = pd.Timestamp("2024-11-30")
 
-# Train the DeepAR model
-def train_deepar_model(df_train, ts_code, start_date, freq, prediction_length):
-    estimator = DeepAREstimator(
-        freq=freq,
+
+
+def train_sff_model(df_train, ts_code, start_date, freq, prediction_length):
+    estimator = SimpleFeedForwardEstimator(
         prediction_length=prediction_length,
-        num_layers=2,
-        hidden_size=32,
-        lr=0.001,
-        weight_decay=1e-08,
-        dropout_rate=0.1,
-        patience=10,
-        num_feat_dynamic_real=0,
-        num_feat_static_cat=1,
-        num_feat_static_real=0,
-        cardinality=[len(np.unique(ts_code))],
-        embedding_dimension=None,
-        scaling=True,
-        default_scale=None,
-        lags_seq=None,
-        time_features=None,
-        num_parallel_samples=100,
-        batch_size=32,
-        num_batches_per_epoch=50,
-        imputation_method=None,
-        trainer_kwargs={"max_epochs": 5},
-        train_sampler=None,
-        validation_sampler=None,
-        nonnegative_pred_samples=False,
+        context_length=10 * prediction_length,  # Default value
+        hidden_dimensions=[20, 20],             # Default hidden layer sizes
+        lr=0.001,                               # Learning rate
+        weight_decay=1e-8,                      # Weight decay regularization
+        batch_norm=False,                       # Whether to apply batch normalization
+        batch_size=32,                          # Batch size for training
+        num_batches_per_epoch=50,               # Number of batches per epoch
+        trainer_kwargs={"max_epochs": 5},       # Trainer configurations
     )
 
     train_ds = create_list_dataset(df_train, ts_code, start_date, freq)
     predictor = estimator.train(training_data=train_ds)
     return predictor
 
-# Process results
-def process_deepar_results(tss, forecasts, df_input, start_test, freq, prediction_length, sku):
+
+def process_sff_results(tss, forecasts, df_input, start_test, freq, prediction_length, sku):
     all_results = []
 
     for i, (tss_series, forecast) in enumerate(zip(tss, forecasts)):
         latest_tss = tss_series.iloc[-prediction_length:].values.flatten()
-        predictions_mean = forecast.mean
-        predictions_median = forecast.median
-        pdv_codigo_name = df_input.columns[i + 1]
 
         results = pd.DataFrame({
             'date': pd.date_range(start=start_test, periods=prediction_length, freq=freq),
             'cant_vta': latest_tss,
-            'cant_vta_pred_deepar_mean': predictions_mean,
-            'cant_vta_pred_deepar_median': predictions_median,
-            'pdv_codigo': pdv_codigo_name,
+            'cant_vta_pred_sff': forecast.mean,
+            'pdv_codigo': df_input.columns[i + 1],
             'codigo_barras_sku': sku
         })
         all_results.append(results)
@@ -88,8 +67,9 @@ def process_deepar_results(tss, forecasts, df_input, start_test, freq, predictio
 
     return final_results
 
+
 # Main function
-def deepar_main(features):
+def sff_main(features):
 
     set_random_seed(42)
 
@@ -130,7 +110,7 @@ def deepar_main(features):
 
       # Train the model
       try:
-          predictor = train_deepar_model(
+          predictor = train_sff_model(
               df_train=df_train,
               ts_code=ts_code,
               start_date=START_TRAIN,
@@ -151,7 +131,7 @@ def deepar_main(features):
       )
 
       # Process results
-      final_results = process_deepar_results(
+      final_results = process_sff_results(
           tss=tss,
           forecasts=forecasts,
           df_input=df_input,
@@ -167,7 +147,6 @@ def deepar_main(features):
 
     combined_results = pd.concat(all_final_results, ignore_index=True)
     return combined_results
-
 
 if __name__ == "__main__":
     # Constants
@@ -193,5 +172,5 @@ if __name__ == "__main__":
     filtered = filtered[filtered['codigo_barras_sku'].isin(filter)]
 
 
-    final_results = deepar_main(filtered)
+    final_results = sff_main(filtered)
     print(final_results)
