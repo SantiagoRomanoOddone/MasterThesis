1f44ea013d5b3ddcdece5e3f449634e2f6b22684
SantiagoRomanoOddone <santiagoromano15@gmail.com>
2025-04-04 15:16:10 -0300

    testing results

--- models/main.py
+++ models/main.py
@@ -1,12 +1,12 @@
-from models.catboost_model.catboost_main import catboost, catboost_by_product
+# from models.catboost_model.catboost_main import catboost, catboost_by_product
 from models.mean_sale.mean_sale_main import mean_sale
 # from models.xgboost.xgboost_main import xgboost, xgboost_by_product
 # from models.deep_learning.lstm.lstm_main import lstm
 # from models.lightgbm.lightgbm_main import lightgbm, lightgbm_by_product
 from train.splits.fixed_split import fixed_split
 
-from models.deep_learning.deepar.deepar import deepar_main
-from models.deep_learning.temporal_fusion_transformer.temporal_fusion_transformer import tft_main
+# from models.deep_learning.deepar.deepar import deepar_main
+# from models.deep_learning.temporal_fusion_transformer.temporal_fusion_transformer import tft_main
 
 # testing
 from models.deep_learning.d_linear.d_linear import dlinear_main
@@ -26,13 +26,13 @@ if __name__ == '__main__':
     np.random.seed(42) 
 
     # Constants
-    CLUSTER_NUMBER = 2
+    CLUSTER_NUMBER = 0
     FREQ = "D"
     PREDICTION_LENGTH = 30
     START_TRAIN = pd.Timestamp("2022-12-01")
     START_TEST = pd.Timestamp("2024-11-01")
     END_TEST = pd.Timestamp("2024-11-30")
-    MODEL = 'tft'
+    MODEL = 'sff'
 
     DATA_PATH = "/Users/santiagoromano/Documents/code/MasterThesis/features/processed/cleaned_features.parquet"
 
@@ -41,9 +41,13 @@ if __name__ == '__main__':
     features = features.sort_values(["pdv_codigo", "codigo_barras_sku", "fecha_comercial"]).reset_index(drop=True)
     features = features[features["cluster_sku"] == CLUSTER_NUMBER]
 
-    # Randomly select 10 SKUs
-    random_skus = np.random.choice(features['codigo_barras_sku'].unique(), size=10, replace=False)
-    features = features[features['codigo_barras_sku'].isin(random_skus)]
+    # # Randomly select 10 SKUs
+    # random_skus = np.random.choice(features['codigo_barras_sku'].unique(), size=10, replace=False)
+    # features = features[features['codigo_barras_sku'].isin(random_skus)]
+    
+    # Filter BY number of stores associated with the SKU
+    filter = features.groupby('codigo_barras_sku').agg({'pdv_codigo': 'nunique'}).reset_index().sort_values('pdv_codigo', ascending=False).head(10)['codigo_barras_sku'].tolist()
+    features = features[features['codigo_barras_sku'].isin(filter)]
 
     train_df, test_df = fixed_split(features)
 
@@ -52,11 +56,11 @@ if __name__ == '__main__':
 
     # mean_sale_results = mean_sale(features)
     # deepar_results = deepar_main(features,CLUSTER_NUMBER)
-    tft_results = tft_main(features,CLUSTER_NUMBER)
+    # tft_results = tft_main(features,CLUSTER_NUMBER)
     # d_linear_results = dlinear_main(features)
     # deep_npts_results = deepnpts_main(features)
     # patch_tst_results = patchtst_main(features)
-    # simple_feedforward_results = sff_main(features,CLUSTER_NUMBER)
+    simple_feedforward_results = sff_main(features,CLUSTER_NUMBER)
     # wavenet_results = wavenet_main(features)
 
     
@@ -82,11 +86,11 @@ if __name__ == '__main__':
     # test_df = pd.merge(test_df, lgbm_results_sku, on=['pdv_codigo', 'codigo_barras_sku', 'fecha_comercial','cant_vta'], how='left')
     # test_df = pd.merge(test_df, mean_sale_results, on=['pdv_codigo', 'codigo_barras_sku', 'fecha_comercial','cant_vta'], how='left')
     # test_df = pd.merge(test_df, deepar_results, on=['pdv_codigo', 'codigo_barras_sku', 'fecha_comercial'], how='left')
-    test_df = pd.merge(test_df, tft_results, on=['pdv_codigo', 'codigo_barras_sku', 'fecha_comercial'], how='left')
+    # test_df = pd.merge(test_df, tft_results, on=['pdv_codigo', 'codigo_barras_sku', 'fecha_comercial'], how='left')
     # test_df = pd.merge(test_df, d_linear_results, on=['pdv_codigo', 'codigo_barras_sku', 'fecha_comercial'], how='left')
     # test_df = pd.merge(test_df, deep_npts_results, on=['pdv_codigo', 'codigo_barras_sku', 'fecha_comercial'], how='left')
     # test_df = pd.merge(test_df, patch_tst_results, on=['pdv_codigo', 'codigo_barras_sku', 'fecha_comercial'], how='left')
-    # test_df = pd.merge(test_df, simple_feedforward_results, on=['pdv_codigo', 'codigo_barras_sku', 'fecha_comercial'], how='left')
+    test_df = pd.merge(test_df, simple_feedforward_results, on=['pdv_codigo', 'codigo_barras_sku', 'fecha_comercial'], how='left')
     # test_df = pd.merge(test_df, wavenet_results, on=['pdv_codigo', 'codigo_barras_sku', 'fecha_comercial'], how='left')
 
 
